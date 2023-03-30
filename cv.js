d06319af3418b3728839bb9c1778d5f756d0f662
verofab <103671727+verofab@users.noreply.github.com>
2023-03-30 16:51:33 -0300

    Initial commit

---
+++ cv.js
@@ -0,0 +1,38 @@
+let boton1 = document.getElementById("boton1");
+let info1 = document.getElementById("info1");
+
+boton1.addEventListener("click", toggleTexto1);
+    
+function toggleTexto1(){
+    info1.classList.toggle("mostrar")
+};
+
+let boton2 = document.getElementById("boton2");
+let info2 = document.getElementById("info2");
+
+boton2.addEventListener("click", toggleTexto2);
+    
+function toggleTexto2(){
+    info2.classList.toggle("mostrar")
+};
+
+let boton3 = document.getElementById("boton3");
+let info3 = document.getElementById("info3");
+
+boton3.addEventListener("click", toggleTexto3);
+    
+function toggleTexto3(){
+    info3.classList.toggle("mostrar")
+};
+
+let boton4 = document.getElementById("boton4");
+let info4 = document.getElementById("info4");
+
+boton4.addEventListener("click", toggleTexto4);
+    
+function toggleTexto4(){
+    info4.classList.toggle("mostrar")
+};
+
+
+
